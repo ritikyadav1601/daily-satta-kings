e0fd7d94664f4139b1f620600e1399d7d6201c98
Ritik yadav <ritikyadav@Ritiks-MacBook-Air.local>
2026-06-01 19:21:28 +0530

    added meta verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Daily Satta Kings – Today Satta Result, Satta King Chart & A7 Satta Updates",
   description: "Check Daily Satta Kings for today satta result, latest satta king chart, and A7 satta updates. Get fast, clear, and updated satta king information in one place.",
+  verification: {
+    google: "-_zSyIAFwdktFE8moDHkJthsN8t_cOsrv381yA82CLs",
+  },
   icons: {
     icon: "/favicon.ico",
   }
